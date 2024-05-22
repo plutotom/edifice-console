0e27a5d105c28c858150b6d0cfc0c874acd3aac4
plutotom <isaiahd.proctor@gmail.com>
2024-05-21 20:22:11 -0500

    notiontodos added

--- src/actions/getWttrData.ts
+++ src/actions/getWttrData.ts
@@ -3,12 +3,21 @@ export const getWttrData = async (city: string) => {
     ? "http"
     : "https" || "http";
 
+  console.log(
+    "Using" +
+      httpsOrNot +
+      "://" +
+      process.env.VERCEL_URL +
+      "/api/weather/wwtr?city=" +
+      city,
+  );
+
   const data = await fetch(
     `${httpsOrNot}://${process.env.VERCEL_URL}/api/weather/wwtr?city=${city}`,
   );
   if (!data.ok) {
     throw new Error("Failed to fetch data");
   }
-  console.log("Called action here" + new Date().toLocaleTimeString());
+  // console.log("Called action here" + new Date().toLocaleTimeString());
   return data.json();
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,22 @@ import { DEFAULT_LOCATION } from "../lib/config";
 // import CurrentWeather from "../components/weeather/CurrentWeather";
 // import type { Weather } from "~/lib/wttrTypes";
 import Time from "../components/ui/Time";
-// import getAllNotionTodos from "~/actions/getNotionTodos";
+import getAllNotionTodos from "~/actions/getNotionTodos";
 export const dynamic = "force-dynamic"; // makes sure changes update on page if database data changes
-// import Todos from "~/components/Todos/Todos";
+import Todos from "~/components/Todos/Todos";
 import CalendarWeekViewV3 from "~/components/Calendar/CalendarWeekViewV3";
 
 export default async function HomePage() {
   const city = DEFAULT_LOCATION.city || "Elgin";
   // const wttrData = (await getWttrData(city)) as Weather;
 
-  // const notionTodos = await getAllNotionTodos();
+  const notionTodos = await getAllNotionTodos();
   return (
     <main className="p-3">
       <Time />
       <div> {new Date().toLocaleTimeString()}</div>
       {/* <div>{wttrData && <CurrentWeather data={wttrData} />}</div> */}
-      {/* <Todos data={[...notionTodos]} /> */}
+      <Todos data={[...notionTodos]} />
       <div>
         <CalendarWeekViewV3 />
       </div>
